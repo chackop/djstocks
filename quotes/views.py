98ec47ccf9f3d0912084e51e69aec299e33f3fcc
Chacko <chackop@gmail.com>
2020-07-25 00:25:53 +0100

    added bootstrap and api check

--- quotes/views.py
+++ quotes/views.py
@@ -4,7 +4,17 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+    api_request = requests.get(
+        "https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_c6e36757831f4bb287427656aec5840e")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as identifier:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
