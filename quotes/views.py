f89419dcab73ca6fb88f5ec509b89b11f657343c
Chacko <chackop@gmail.com>
2020-07-25 15:30:57 +0100

    added ticker symbol

--- quotes/views.py
+++ quotes/views.py
@@ -6,15 +6,21 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
-    api_request = requests.get(
-        "https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_c6e36757831f4bb287427656aec5840e")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as identifier:
-        api = "Error..."
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
 
-    return render(request, 'home.html', {'api': api})
+        api_request = requests.get(
+            "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_c6e36757831f4bb287427656aec5840e")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as identifier:
+            api = "Error..."
+        return render(request, 'home.html', {'api': api})
+
+    else:
+        return render(request, 'home.html', {'ticker': "Enter ticker symbol"})
 
 
 def about(request):
