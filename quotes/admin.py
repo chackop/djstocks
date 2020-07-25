176e18a56370103df977b2697bd9826f8ecd3ffe
Chacko <chackop@gmail.com>
2020-07-25 16:42:10 +0100

    applied migration for ticker symbol

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Stock
 
 # Register your models here.
+
+
+# class StockAdmin(admin.ModelAdmin):
+#     pass
+
+
+# admin.site.register(Stock, StockAdmin)
+admin.site.register(Stock)
--- quotes/urls.py
+++ quotes/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+
+
+admin.autodiscover()
 
 urlpatterns = [
     path('', views.home, name="home"),
     path('about.html', views.about, name="about"),
+    path('add_stock.html', views.add_stock, name="add_stock"),
 ]
